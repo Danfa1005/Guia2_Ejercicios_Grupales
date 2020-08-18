735fddd34f9b55f905c68e0c29cb0b2f90f6e05d
JesusMG17 <68879190+JesusMG17@users.noreply.github.com>
2020-08-17 20:30:56 -0500

    Add files via upload

---
+++ Ejercicio4.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Ejercicio3
+
+{
+	public static void main(String[] args) {
+        
+        int cant, num, pares=0, impares=0;         
+        System.out.print("Cuantos numeros desea ingresar: ");
+        Scanner scanner = new Scanner(System.in);
+        cant = scanner.nextInt();
+        
+        for(int i=1; i<=cant; i++){
+            System.out.print("Ingresar numero " + i + " de " + cant + ":  ");
+            num =  (new Scanner(System.in)).nextInt();
+            if(num%2 == 0)
+                 pares++;
+            else
+                 impares++;             
+        }
+        System.out.println("\nCantidad de numeros pares: " + pares);
+        System.out.println("Cantidad de numeros impares: " + impares);         
+    }       
+}
